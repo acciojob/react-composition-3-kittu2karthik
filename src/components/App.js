e5432609c21e30b3fd5e2a528d41cab319200881
kittu2karthik <kittu2karthik@gmail.com>
2025-11-28 14:31:22 +0530

    completed the requirements

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import Tooltip from "./Tooltip";
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className='App'>
+      <Tooltip text="This is an amazing feature!">
+        <p>Hover over me</p>
+      </Tooltip>
+
+      <Tooltip text="I am a tooltip for this button">
+        <p>Hover here me to see another tooltip</p>
+      </Tooltip>
     </div>
   )
 }
---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,16 @@
+function Tooltip({ text, children }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={() => setIsVisible(true)}
+      onMouseLeave={() => setIsVisible(false)}
+    >
+      {children}
+      {isVisible && <span className="tooltiptext">{text}</span>}
+    </div>
+  );
+}
+
+export default Tooltip;
